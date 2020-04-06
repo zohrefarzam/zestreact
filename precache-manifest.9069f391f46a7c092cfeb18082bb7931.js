6ae0495e7e205cf93b2beb1626235c247ce4697b
zohre <zohrefarzam@gmail.com>
2020-04-06 11:57:51 -0700

    Updates

---
+++ precache-manifest.9069f391f46a7c092cfeb18082bb7931.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "557c102556347ca78d7fb3dda2ea1ac7",
+    "url": "/zestreact/static/media/widget-blog.557c1025.jpg"
+  },
+  {
+    "revision": "954edfbe8f853600184d",
+    "url": "/zestreact/static/css/main.e8fc385a.chunk.css"
+  },
+  {
+    "revision": "e026bdc980e615bd758a",
+    "url": "/zestreact/static/js/runtime~main.1eb79d0d.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/zestreact/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "729f7404a610131db4ac",
+    "url": "/zestreact/static/js/2.84296a6d.chunk.js"
+  },
+  {
+    "revision": "42ac80550015eecba1e8db7a5ea644ba",
+    "url": "/zestreact/static/media/menu-image3.42ac8055.jpg"
+  },
+  {
+    "revision": "1e87a75387cd7673a2b1f6f61e2d9c17",
+    "url": "/zestreact/static/media/topbar-image4.1e87a753.jpg"
+  },
+  {
+    "revision": "2bd412296331cbbba20cf2d208e4b426",
+    "url": "/zestreact/static/media/topbar-image1.2bd41229.jpg"
+  },
+  {
+    "revision": "535309038a347d80db412e7911e1f006",
+    "url": "/zestreact/static/media/topbar-image2.53530903.jpg"
+  },
+  {
+    "revision": "38e60ed4eb3fcde81ecfe841678e088f",
+    "url": "/zestreact/static/media/topbar-image3.38e60ed4.jpg"
+  },
+  {
+    "revision": "abe950513757efc57f6b40f82a27be8f",
+    "url": "/zestreact/static/media/menu-image4.abe95051.jpg"
+  },
+  {
+    "revision": "7ff2e995078ec6db99ef4cd6beb67a98",
+    "url": "/zestreact/static/media/menu-image1.7ff2e995.jpg"
+  },
+  {
+    "revision": "dc299aa2a7afc2e2f808ca79dd461826",
+    "url": "/zestreact/static/media/menu-image2.dc299aa2.jpg"
+  },
+  {
+    "revision": "954edfbe8f853600184d",
+    "url": "/zestreact/static/js/main.2031cbbd.chunk.js"
+  },
+  {
+    "revision": "15a3dc478f4c083c588f75ef987abfd6",
+    "url": "/zestreact/static/media/login-bg.15a3dc47.jpg"
+  },
+  {
+    "revision": "e6a6e9604912b934bbee97a7971db77e",
+    "url": "/zestreact/static/media/widget-weather.e6a6e960.jpg"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/zestreact/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/zestreact/static/media/Simple-Line-Icons.f33df365.eot"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/zestreact/static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/zestreact/static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "2fe2efe63441d830b1acd106c1fe8734",
+    "url": "/zestreact/static/media/Simple-Line-Icons.2fe2efe6.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/zestreact/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/zestreact/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/zestreact/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/zestreact/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "729f7404a610131db4ac",
+    "url": "/zestreact/static/css/2.b86f3a6d.chunk.css"
+  },
+  {
+    "revision": "5c70777c2feffbb037b964aa023f4f16",
+    "url": "/zestreact/index.html"
+  }
+];
